fee62bf3fdbe1da7e56dd380ca7e16a68ac91042
idbd <idbd.young@gmail.com>
2021-06-09 12:23:44 +0900

    week1 rightSideView problem-setting

---
+++ archive/week1/test/idbdyoung-rightSideView.test.js
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import rightSideView from "../lib/idbdyoung-rightSideView.js";
+
+describe("permutation", function() {
+  it("should return all possible combinations", function() {
+    expect(rightSideView().to.eql());
+  });
+});
