8406e0722736d67727155a3d48622b606f358a27
gumsanim <79829538+gumsanim@users.noreply.github.com>
2021-06-13 09:47:37 +0900

    Add files via upload

---
+++ test/01.idbdyoung-targetNumber.test.js
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import solution from "../lib/01.idbdyoung-targetNumber";
+
+describe("solution", function() {
+  it("basic case", function() {
+    expect(solution([1,1,1,1,1])).to.eql(5);
+  });
+});
