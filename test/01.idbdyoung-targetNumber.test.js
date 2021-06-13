fcc1130f1a88e8a5a808aaf7c7a3540d92d93d60
idbd <idbd.young@gmail.com>
2021-06-13 15:09:05 +0900

    1주차 수정

--- test/01.idbdyoung-targetNumber.test.js
+++
@@ -1,8 +0,0 @@
-import { expect } from "chai";
-import solution from "../lib/01.idbdyoung-targetNumber";
-
-describe("solution", function() {
-  it("basic case", function() {
-    expect(solution([1,1,1,1,1],3)).to.eql(5);
-  });
-});
