8ddfe0a8fedf945e70185afef55155a1c300157a
idbd <idbd.young@gmail.com>
2021-06-13 15:07:16 +0900

    1주차 문제풀이

--- test/01.idbdyoung-rightSideView.test.js
+++ test/01.idbdyoung-rightSideView.test.js
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import rightSideView from "../lib/01.idbdyoung-rightSideView";
 
 describe("rightSideView", function() {
-  it("basic case", function() {
-    expect(rightSideView(1)).to.eql(1);
+  it("basic case or edge case", function() {
+    expect(rightSideView([1,2,3,null,5,null,4])).to.eql([1,3,4]);
   });
 });
