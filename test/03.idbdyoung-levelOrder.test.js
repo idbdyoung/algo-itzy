ca29c413dd1ac14c210004a989892d0e50bcfda9
idbd <idbd.young@gmail.com>
2021-06-27 12:43:28 +0900

    week3 levelOrder problem-solved

--- test/03.idbdyoung-levelOrder.test.js
+++ test/03.idbdyoung-levelOrder.test.js
@@ -5,10 +5,16 @@ describe("levelOrder", function() {
   it("basic case", function() {
     expect(levelOrder([3,9,20,null,null,15,7])).to.eql([[3],[9,20],[15,7]]);
   });
+  it("basic case", function() {
+    expect(levelOrder([1,2,null,3,null,null,7,8,9,10,11,12,13])).to.eql([[1],[2],[3],[7],[8,9],[10,11,12,13]]);
+  });
+  it("basic case", function() {
+    expect(levelOrder([3,9,20,null,null,15,7])).to.eql([[3],[9,20],[15,7]]);
+  });
   it("edge case", function() {
     expect(levelOrder([1])).to.eql([[1]]);
   });
   it("edge case", function() {
-    expect(levelOrder([])).to.eql([[]]);
+    expect(levelOrder([])).to.eql([]);
   });
 });
