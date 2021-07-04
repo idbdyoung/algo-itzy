2f26acc0799404bed4f7bb7f2da28317ac008d3c
idbd <idbd.young@gmail.com>
2021-07-04 15:57:46 +0900

    week4 zigzagLevelOrder problem-solved

--- test/04.idbdyoung-zigzagLevelOrder.test.js
+++ test/04.idbdyoung-zigzagLevelOrder.test.js
@@ -5,4 +5,7 @@ describe("zigzagLevelOrder", function() {
   it("basic case", function() {
     expect(levelOrder([3,9,20,null,null,15,7])).to.eql([[3],[20,9],[15,7]]);
   });
+  it("basic case", function() {
+    expect(levelOrder([3,9,20,null,null,15,7,12,13,14,15])).to.eql([[3],[20,9],[15,7],[15,14,13,12]]);
+  });
 });
