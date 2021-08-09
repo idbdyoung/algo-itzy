08b20f31fd857b9f8b66371d47927b6ca88da5ea
idbd <idbd.young@gmail.com>
2021-08-09 09:11:36 +0900

    remove user gumsnim

--- test/04.gumsanim-maximumUnitsOnATruck.test.js
+++
@@ -1,8 +0,0 @@
-import { expect } from "chai";
-import maximumUnits from "../lib/04.gumsanim-maximumUnitsOnATruck";
-
-describe("maximumUnits", function() {
-  it("basic case", function() {
-    expect(maximumUnits([[1,3],[2,2],[3,1]],4)).to.eql(8);
-  });
-});
