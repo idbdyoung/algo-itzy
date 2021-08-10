795c19c6603ce7ed926ba6b1592c55b10c41f80d
idbd <idbd.young@gmail.com>
2021-08-10 10:22:38 +0900

    week6 searchInsert problem-solved

--- test/06.searchInsert.test.js
+++ test/06.searchInsert.test.js
@@ -2,7 +2,19 @@ import { expect } from "chai";
 import searchInsert from "../lib/06.searchInsert";
 
 describe("searchInsert", function() {
-  // it("basic case", function() {
-  //   expect(searchInsert()).to.eql();
-  // });
+  it("basic case", function() {
+    expect(searchInsert([1,3,5,6], 5)).to.eql(2);
+  });
+  it("basic case", function() {
+    expect(searchInsert([1,3,5,6], 2)).to.eql(1);
+  });
+  it("basic case", function() {
+    expect(searchInsert([1,3,5,6], 7)).to.eql(4);
+  });
+  it("basic case", function() {
+    expect(searchInsert([1,3,5,6], 0)).to.eql(0);
+  });
+  it("edge case", function() {
+    expect(searchInsert([1], 0)).to.eql(0);
+  });
 });
