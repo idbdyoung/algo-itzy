7b6f7f79da4485f62405ebb552e65663a3588516
idbd <idbd.young@gmail.com>
2021-08-10 10:25:38 +0900

    week7 pivotIndex problem-setting

---
+++ test/pivotIndex.test.js
@@ -0,0 +1,14 @@
+import { expect } from "chai";
+import pivotIndex from "../lib/07.pivotIndex";
+
+describe("pivotIndex", function() {
+  it("basic case", function() {
+    expect(pivotIndex([1,7,3,6,5,6])).to.eql(3);
+  });
+  it("basic case", function() {
+    expect(pivotIndex([1,2,3])).to.eql(-1);
+  });
+  it("basic case", function() {
+    expect(pivotIndex([2,1,-1])).to.eql(0);
+  });
+});
