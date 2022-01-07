c8ebfbc31fe58bf15f51d7188eccea978b07e397
idbd <idbd.young@gmail.com>
2022-01-07 16:49:21 +0900

    day4 신규아이디추천 problem setting

---
+++ test/c4.신규아이디추천.test.js
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+import solution from "../lib/c4.신규아이디추천";
+
+describe("palindrome number", function() {
+  it("basic case", function() {
+  });
+  it("edge case", function() {
+  });
+});
